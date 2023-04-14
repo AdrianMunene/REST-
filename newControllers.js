379bffba89d9819beea298e8e33a01b941b80191
AdrianMunene <adrianmunene62@gmail.com>
2023-04-14 22:46:48 +0300

    Unique validation on bookings table

--- newControllers.js
+++ newControllers.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 
 const schema = fs.readFileSync('./data.schema.json', 'utf8');
 const schemaObj = JSON.parse(schema);
+
 const dataObj = {};
 Object.keys(schemaObj).forEach(table => {
     dataObj[table] = [];
@@ -17,11 +18,13 @@ const createClient = (req, res) =>{
     const {error} = requestSchema.validate(req.body);
     if (error){
         return res.status(400).json({success: false, message: 'Request body contains invalid arguments'})
-    }
+    };
+
     const identical = dataObj["clients"].find((identical)=> identical.clientId === req.body.clientId);
     if(identical){
         return res.status(400).json({success: false, message: 'client with given ID already exists'});
     };
+
     dataObj["clients"].push(req.body);
     const data = JSON.stringify(dataObj);
     fs.writeFileSync('./data.json', data);
@@ -42,7 +45,9 @@ const createRoom = (req, res) =>{
     if(identical){
         return res.status(400).json({success: false, message: 'room with given ID already exists'});
     };
+
     dataObj["rooms"].push(req.body);
+
     const data = JSON.stringify(dataObj);
     fs.writeFileSync('./data.json', data);
     res.status(201).json({success: true, data: dataObj["rooms"]});
@@ -56,15 +61,20 @@ const createBooking = (req, res) =>{
     const booking = req.body;
     if(!req.body.clientId || !req.body.roomId){
         return res.status(400).json({success: false, message: 'Please provide an room and client ID'});
-    }
-    //find corresponding roomId and clientId for the booking you're trying to create
+    };
+
     const clientLog = dataObj["clients"].find((clientLog)=> clientLog.clientId  === booking.clientId);
     const roomLog = dataObj["rooms"].find((roomLog)=> roomLog.roomId === booking.roomId);
     if(!clientLog || !roomLog){
         return res.status(400).json({success: false, message: 'room or client ID does not exist'});
-    }
-    //Add bookingId and checkInTime to booking object
+    };
     booking.bookingId = Number(req.body.clientId.toString() + req.body.roomId.toString());
+    
+    const identical = dataObj["bookings"].find((identical)=> identical.bookingId === booking.bookingId);
+    if(identical){
+        return res.status(400).json({success: false, message: 'room and client have already been assigned'});
+    };
+
     booking.checkInTime = new Date().toLocaleString();
 
     dataObj["bookings"].push(booking);
@@ -80,7 +90,9 @@ const updateBoooking = (req, res) =>{
         return res.status(404).json({success: false, message: `no booking exists with ID ${id} `});
     }
     delete dataObj["bookings"].find((bookingLog)=> bookingLog.bookingId === id);
+
     bookingLog.checkOutTime = new Date().toLocaleString();
+
     const data = JSON.stringify(dataObj);
     fs.writeFileSync('./data.json', data);
     res.status(201).json({success: true, data: dataObj["bookings"]});
